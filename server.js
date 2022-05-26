24d68981e68b1dcccae5e5726e58f4ff024915c5
gil926 <82399605+gil926@users.noreply.github.com>
2022-05-27 00:08:38 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const app = express()
-const ejs = require('ejs')
+const ejs = require('ejs');
 
+app.set('view engine','ejs');
 
-app.set("view engine",'ejs')
 app.get('/', function (req, res) {
-  res.render('index')
-})
-
+    res.render('index')
+  })
+  
 app.use(express.static('images'))
 
 app.listen(3000)
